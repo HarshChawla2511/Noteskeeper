727f18f30d60907d1fc62b1cef28ac2f9e15e146
Harsh Chawla <harshchawla989@gmail.com>
2023-06-05 02:59:25 +0530

    1st commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+function Note(props) {
+
+  function handleClick(){
+    props.onDelete(props.id)
+  }
+
+  return (
+    <div className="note">
+      {/* Html attributes can only be applied to html elements not to our custom ones. */}
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick = {handleClick}>
+        <DeleteIcon />
+      </button>
+    </div>
+  );
+}
+
+export default Note;
