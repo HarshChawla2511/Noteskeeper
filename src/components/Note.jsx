376b85db0e7bf37ac0a5e4b9141972b5397c46f5
harshchawla2511 <harshchawla989@gmail.com>
2023-07-13 22:35:01 +0530

    final one

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 
+
 function Note(props) {
 
   function handleClick(){
@@ -12,7 +13,6 @@ function Note(props) {
       {/* Html attributes can only be applied to html elements not to our custom ones. */}
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-=======
       <button onClick = {handleClick}>
         <DeleteIcon />
       </button>
