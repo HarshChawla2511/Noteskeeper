41159871ac982032206ce908f52377dbc0fb86ca
harshchawla2511 <harshchawla989@gmail.com>
2023-07-13 22:25:41 +0530

    THis  is the final

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,6 @@ import EventNoteIcon from "@mui/icons-material/EventNote";
 function Header(){
     
     return <header>
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <h1><EventNoteIcon/> Noteskeeper</h1>
-=======
-        <h1><EventNoteIcon/> Notekeeper</h1>
->>>>>>> 727f18f30d60907d1fc62b1cef28ac2f9e15e146
     </header>
 
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
-<<<<<<< HEAD
-import Fab from "@mui/material/Fab";
-import Zoom from "@mui/material/Zoom";
-=======
->>>>>>> bf7b688 (Final)
 
 function Note(props) {
 
@@ -17,26 +12,12 @@ function Note(props) {
       {/* Html attributes can only be applied to html elements not to our custom ones. */}
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-<<<<<<< HEAD
-      <Zoom in={true}>
-        <Fab onClick={handleClick}>
-          <DeleteIcon />
-        </Fab>
-      </Zoom>
-    </div>
-  )
 =======
-=======
->>>>>>> bf7b688 (Final)
       <button onClick = {handleClick}>
         <DeleteIcon />
       </button>
     </div>
   );
-<<<<<<< HEAD
->>>>>>> 727f18f30d60907d1fc62b1cef28ac2f9e15e146
-=======
->>>>>>> bf7b688 (Final)
 }
 
 export default Note;
